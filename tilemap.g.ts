7a3250e1945ffab58a2567aa7c08ebb624e3c2bf
ksavage-work <67298646+ksavage-work@users.noreply.github.com>
2020-09-02 12:42:58 -0700

    starting lvl2

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -14,6 +14,12 @@ namespace myTiles {
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
