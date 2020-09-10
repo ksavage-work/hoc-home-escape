fdccb54786fe69415e2b4f5c7f460d2b57f7920f
ksavage-work <67298646+ksavage-work@users.noreply.github.com>
2020-09-10 16:31:42 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,3 @@
-// controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-//     if (hoc.near_bathroom_sink()) {
-//         hoc.fix_sink()
-//     } else if (hoc.near_toilet()) {
-//         hoc.fix_toilet()
-//     } else if (hoc.near_shower()) {
-//         hoc.fix_shower()
-//     } else if (hoc.near_kitchen_sink()) {
-//         for (let index = 0; index < 5; index++) {
-//             hoc.clean_dishes()
-//         }
-//     }
-// })
-// controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-//     if (hoc.near_fan()) {
-//         hoc.fan_blows_air()
-//     }
-// })
-// hoc.near_door(function () {
-//     hoc.pick_lock()
-// })
+hoc.near_door(function() {
+    
+})
